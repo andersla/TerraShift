526e749e0a71bdaceb54c62981e973f723736a35
Anders Larsson <anders.larsson@icm.uu.se>
2017-10-18 14:23:05 +0200

    added packer openstack template

---
+++ packer/packer-openstack-snic-variables.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Read password
+export OS_PASSWORD=""
+
+#Set the rest
+export OS_USERNAME=""
+export OS_AUTH_URL=""
+export OS_USER_DOMAIN_ID=""
+export OS_DOMAIN_ID="same as user domain id usually"
+export OS_REGION_NAME=""
+export OS_PROJECT_ID=""
+export OS_TENANT_ID=“same as project id usually”
+export OS_TENANT_NAME=""
+export OS_AUTH_VERSION=3
+
+export OS_FLAVOR=""
+export OS_NETWORK=""
+export OS_POOL_NAME=""
+export OS_EXTERNAL_NET_UUID=""
+export OS_SOURCE_IMAGE_NAME=""
+export IMAGE_NAME="kubenow-"
+
+export OS_INTERFACE=public
