7c9ef6480194839a920bafe2d3391c8b8b52ec41
Cloud User <centos@anders-deployer.openstacklocal>
2017-10-24 13:24:25 +0000

    adds hostname in bootstrap script

--- bootstrap/openshift.sh
+++ bootstrap/openshift.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 ## Add hostname
-#echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
+IP=$(hostname -I | cut -d ' ' -f1 | sed 's/\./-/g')
+HOSTNAME=host-$IP
 
+hostname $HOSTNAME
+echo $HOSTNAME > /etc/hostname
+echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
